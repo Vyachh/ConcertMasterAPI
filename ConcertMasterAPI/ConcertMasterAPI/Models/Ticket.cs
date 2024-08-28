7b8e8630a8bc10bce4190faa83e3edd1a2103109
vyachhw <vyachhw@gmail.com>
2024-08-28 15:18:29 +0300

    Models Added

---
+++ ConcertMasterAPI/ConcertMasterAPI/Models/Ticket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConcertMasterAPI.Models
+{
+    public class Ticket
+    {
+        public Guid Id { get; set; }
+        public TicketType Type { get; set; }
+        public TicketStatus Status { get; set; }
+        public int Price { get; set; }
+        public User Buyer { get; set; }
+    }
+
+    public enum TicketStatus
+    {
+        InStock,
+        Sold
+    }
+
+    public enum TicketType
+    {
+        Default,
+        Vip,
+        Student
+    }
+}
