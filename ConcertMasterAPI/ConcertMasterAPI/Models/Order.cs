39c964634ba6e44c9315dd60fc87729af8c7afd5
vyachhw <vyachhw@gmail.com>
2024-08-28 15:38:40 +0300

    Models Initiated

---
+++ ConcertMasterAPI/ConcertMasterAPI/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConcertMasterAPI.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public User Buyer { get; set; }
+        public DateTime Created { get; set; }
+        public IEnumerable<Ticket> Tickets { get; set; }
+    }
+}
---
+++ ConcertMasterAPI/ConcertMasterAPI/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConcertMasterAPI.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Login { get; set; }
+        public string PasswordHash { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public UserType Type { get; set; }
+    }
+
+    public enum UserType
+    {
+        Buyer,
+        Artist,
+        Organizer,
+        Administator
+    }
+}
