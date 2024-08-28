c6eb1da2329d678b1d0ce7c5e979a9a7430b0eea
vyachhw <vyachhw@gmail.com>
2024-08-28 16:17:39 +0300

    DbContext Added

---
+++ ConcertMasterAPI/ConcertMasterAPI/Context/PgContext.cs
@@ -0,0 +1,23 @@
+﻿using ConcertMasterAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace ConcertMasterAPI.Context
+{
+    public class PgContext : DbContext
+    {
+        public PgContext(DbContextOptions<PgContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AddressVenue> Venues { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<EventCategory> EventCategories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
